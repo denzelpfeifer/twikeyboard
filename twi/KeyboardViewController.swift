cd70debe1dd1157813e580a4f03c6d8dddd47402
K'tyah Point Du Jour <ktyahpoint@ktyahs-macbook-pro.local>
2018-08-14 23:03:33 -0500

    Twi Keyboard

---
+++ twi/KeyboardViewController.swift
@@ -0,0 +1,82 @@
+//
+//  KeyboardViewController.swift
+//  twi
+//
+//  Created by K'tyah Point on 8/14/18.
+//  Copyright © 2018 K'tyah Point. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let nib = UINib(nibName: "KeyboardView", bundle: nil)
+        let objects = nib.instantiate(withOwner: self, options: nil)
+        
+        view = objects[0] as! UIView
+        
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+
+    
+    @IBAction func buttonPressed(button :UIButton){
+        var string = button.titleLabel?.text
+        (textDocumentProxy as UIKeyInput).insertText("\(string!)")
+    }
+    @IBAction func backspacePressed(button :UIButton){
+        (textDocumentProxy as UIKeyInput).deleteBackward()
+    }
+    @IBAction func spacebarPressed(button :UIButton){
+        (textDocumentProxy as UIKeyInput).insertText(" ")
+    }
+    @IBAction func enterBarpressed(button :UIButton){
+        (textDocumentProxy as UIKeyInput).insertText("\n")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+
+}
